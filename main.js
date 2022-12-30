2adf6a5853fb3ceaa37f0670dc939422e9e9ac89
KevinRodriguez1 <code.kevindrr@gmail.com>
2022-12-30 02:32:02 -0500

    Pagina con pushbar, projects, more section y footer base

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+/* ------ BORRAR ESTO ---- 
+jQuery('document').ready(function($){  
+    $('#pushbar').hide();
+})
+/* ------ BORRAR ESTO ---- */
+
+
+
+
+
+
+/* NAV */
 window.addEventListener("scroll", function(){
     var nav = this.document.querySelector("nav");
     nav.classList.toggle("abajo",window.scrollY>0)
+});
+
+/* PUSHBAR*/
+jQuery('document').ready(function($){
+    var barBtn = $('#pb_down');
+
+    barBtn.click(function(){
+        
+        $('#pushbar').hide();
+
+    })
 })
 
+/* MENU NAV */
 jQuery('document').ready(function($){
     var menuBtn = $('#menu'),
         menu = $('nav');
@@ -17,4 +41,5 @@ jQuery('document').ready(function($){
 
     });
 });
+ 
 
